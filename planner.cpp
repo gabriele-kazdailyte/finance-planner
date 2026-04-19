323e08f05846af064bdc4522f62a32850f3c4ae0
Gabrielė Každailytė <gabriele.kazdailyte@mif.stud.vu.lt>
2026-04-19 23:41:12 +0300

    Added: description and amount introduction added to addTransaction() function

--- planner.cpp
+++ planner.cpp
@@ -4,7 +4,7 @@
 struct Transaction {
     std::string date;
     std::string category;
-    std::string desription;
+    std::string description;
     double amount;
 };
 
@@ -16,19 +16,31 @@ void addTransaction() {
     Transaction t;
 
     std::vector<std::string> categories = {"food", "transport", "rent", "income", "personal expenses", "loan payments", "income", "other"};
-    std::cout << "Categories:\n";
+    std::cout << "  Categories:\n";
     for(int i = 0; i < (int)categories.size(); ++i)
         std::cout << "      " << i + 1 << ".     " << categories[i] << "\n";
-    std::cout << "Select a category (1 - " << categories.size() << "):\n";
+    std::cout << "  Select a category (1 - " << categories.size() << "):\n";
     int choice;
     std::cin >> choice;
 
     if (choice < 1 || choice > categories.size()) {
-        std::cout << "Invalid category selected \n" ;
+        std::cout << "  Invalid category selected \n" ;
         return;
     }
 
     t.category = categories[choice - 1];
+
+    std::cout << "  Enter a description: \n";
+    std::getline(std::cin, t.description);
+
+    std::cout << "  Enter the amount (negative = income): \n";
+    if ( !(std::cin >> t.amount) ) {
+        std::cout << "  Invalid amount entered \n";
+        return;
+    }
+
+    transactions.push_back(t);
+    std::cout << "  Transaction saved.\n";
 }
 
 int main()
